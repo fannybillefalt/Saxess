39fe8c441e907b5c0c7d263a139518e01485afca
fannybillefalt <fannybillefalt@gmail.com>
2026-02-03 10:02:09 +0100

    Add project files.

---
+++ Övning-Frisörsalong Saxess/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Övning_Frisörsalong_Saxess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
