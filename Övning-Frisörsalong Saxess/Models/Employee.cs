f2319d80f42a22ed5a5a639eea5e1ca6bd780a15
Viktor <Viktorandersson78@gmail.com>
2026-02-03 10:20:15 +0100

    Create Scaffold

---
+++ Övning-Frisörsalong Saxess/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Övning_Frisörsalong_Saxess.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
---
+++ Övning-Frisörsalong Saxess/Models/Treatment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Övning_Frisörsalong_Saxess.Models;
+
+public partial class Treatment
+{
+    public int Id { get; set; }
+
+    public string Treatmentname { get; set; } = null!;
+
+    public int Duration { get; set; }
+
+    public decimal Price { get; set; }
+
+    public bool? Avaible { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
